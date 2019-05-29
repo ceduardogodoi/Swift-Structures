bb4d392edaf9b34544957fe88e63dc2796e207e2
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-05-28 23:43:06 -0300

    Stored properties, Computed propertypes and Type properties

--- Structures - Start.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Structures - Start.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -1,5 +1,67 @@
 //: [Previous](@previous)
 //: # Properties
+struct Wizard {
+    static var commonMagicalIngredients: [String] {
+        return [
+            "Polijuyce Potion",
+            "Eye of Haystack Needle",
+            "The Force"
+        ]
+    }
 
+    var firstName: String {
+        willSet {
+            print("\(firstName) will be set to \(newValue)")
+        }
+        didSet {
+            if firstName.contains(" ") {
+                print("No spaces allowed! \(firstName) is not a first name. Reverting name to \(oldValue)")
+                firstName = oldValue
+            }
+        }
+    }
+
+    var lastName: String
+
+    var fullName: String {
+        get { return "\(firstName) \(lastName)" }
+
+        // set(newFullName) {
+        set {
+            let nameSubstrings = newValue.split(separator: " ")
+
+            guard nameSubstrings.count >= 2 else {
+                print("\(newValue) is not a full name.")
+                return
+            }
+
+            let nameStrings = nameSubstrings.map(String.init)
+            firstName = nameStrings.first!
+            lastName = nameStrings.last!
+        }
+    }
+
+    lazy var magicalCreature = summonMagicalCreature(summoner: fullName)
+
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+var wizard = Wizard(firstName: "Gendalf", lastName: "Greyjoy")
+wizard.firstName = "Hermione"
+wizard.lastName = "Kenobi"
+wizard.fullName
+
+wizard.fullName = "Severus Wenderlich"
+
+wizard.firstName = "Merlin Rincewind"
+print(wizard.magicalCreature)
+
+wizard.fullName = "Qui-Gon Crayskull"
+print(wizard.magicalCreature)
+
+Wizard.commonMagicalIngredients
 
 //: [Next](@next)
